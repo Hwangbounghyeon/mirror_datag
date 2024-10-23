85dd6b36ef79e5b98d1d555b460eb40a896b308b
SnowySparks <originpc@naver.com>
2024-10-23 15:43:46 +0900

    [#1] test: 폰트 적용 테스트

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@mui/material";
 export default function Home() {
   return (
     <div>
       <p className="text-[30px] font-BMJUA">안녕 Next.js</p>
+      <Button variant="text" color={"inherit"}>
+        안녕
+      </Button>
     </div>
   );
 }
