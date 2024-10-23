5b58244c6a13de8522811bb4a7ec34267466e22b
SnowySparks <originpc@naver.com>
2024-10-23 15:18:11 +0900

    [#1] Config : 주아체, Pretendard 폰트 최적화 적용 및 tailwindCSS 반영

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -1,4 +1,23 @@
 import "./globals.css";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import localFont from "next/font/local";
+import theme from "./../theme";
+
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
+const BMJUA = localFont({
+  src: "./fonts/BMJUA.woff",
+  display: "swap",
+  weight: "400",
+  variable: "--font-bmjua",
+});
 
 export default function RootLayout({
   children,
@@ -6,8 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${pretendard.variable} ${BMJUA.variable}`}>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
---
+++ fe/src/theme.ts
@@ -0,0 +1,10 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Pretendard-Regular, var(--font-roboto)",
+  },
+});
+
+export default theme;
