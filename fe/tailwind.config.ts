65181e0007a9d070e199d5c31bfabb570969c9a7
SnowyPacks <originpc@naver.com>
2024-10-26 22:57:35 +0900

    [#10] Feature : DarkMode tailwind css 적용

--- fe/tailwind.config.ts
+++ fe/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "selector",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +16,28 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        light: {
+          text: {
+            DEFAULT: "var(--light-text)",
+          },
+          background: {
+            DEFAULT: "var(--light-background)",
+          },
+          border: {
+            DEFAULT: "var(--light-border)",
+          },
+        },
+        dark: {
+          text: {
+            DEFAULT: "var(--dark-text)",
+          },
+          background: {
+            DEFAULT: "var(--dark-background)",
+          },
+          border: {
+            DEFAULT: "var(--dark-border)", // 라이트 모드 border 색상 추가
+          },
+        },
       },
     },
   },
