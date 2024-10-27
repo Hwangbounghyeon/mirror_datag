81251b4e5a3a171e4f3f1709c3874c3bdd586f4c
SnowyPacks <originpc@naver.com>
2024-10-27 22:04:52 +0900

    [#10] Feature : Chakra UI Import 및 Sidebar 구현

--- fe/src/app/component/page.tsx
+++ fe/src/app/component/page.tsx
@@ -1,16 +1,25 @@
+"use client";
+
+import Sidebar from "@/components/common/sidebar";
 import React from "react";
 
 const Page = () => {
   return (
-    <div className="flex flex-col justify-center align-middle items-center">
-      <h1 className="mt-3 text-[30px]">Component Page</h1>
-
-      <div className="flex flex-col justify-center align-middle items-center">
-        <p>Button</p>
-        <button>Button Default</button>
-        <p>globals.css 에 적용된 상태</p>
+    <section className="flex flex-row justify-center">
+      <div className="w-[30%]">
+        <Sidebar></Sidebar>
       </div>
-    </div>
+
+      <div className="flex flex-col justify-center align-middle items-center w-full">
+        <h1 className="mt-3 text-[30px]">Component Page</h1>
+
+        <div className="flex flex-col justify-center align-middle items-center">
+          <p>Button</p>
+          <button>Button Default</button>
+          <p>globals.css 에 적용된 상태</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
