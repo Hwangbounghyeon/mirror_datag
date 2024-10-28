0aee58cf205d1508ef5401ca3ac439839779f188
황병현 <grant0808@naver.com>
2024-10-28 17:48:53 +0900

    [#12] feature : mariadb table 설계

--- DL/app/models/mariadb_image.py
+++ DL/app/models/mariadb_image.py
@@ -1,9 +1,58 @@
-from pydantic import BaseModel
-from datetime import datetime
+from configs.mariadb import Base
+from sqlalchemy import Column, ForeignKey, Integer, VARCHAR, DateTime
+from sqlalchemy.orm import relationship
+from enum import Enum, unique
 
-class Image(BaseModel):
-    image_id: int
-    object_id: int
-    feature_id: int
-    created_at: datetime
-    updated_at: datetime
+
+
+@unique
+class TagType(Enum):
+    DATE = "DATE"
+    MODEL = "MODEL"
+    TASK = "TASK"
+    BRANCH = "BRANCH"
+    LOCATION = "LOCATION"
+    EQUIPMENT = "EQUIPMENT"
+    USER = "USER"
+
+
+
+# 이미지 테이블
+class Images(Base):
+    __table__ = "images"
+
+    image_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    metadata_id = Column(VARCHAR(255))
+    feature_id = Column(VARCHAR(255))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    image_tag = relationship("ImageTag", back_populates="images")
+    dataset_image = relationship("DatasetImage", back_populates="images")
+
+
+
+# 태그 테이블
+class Tags(Base):
+    __table__ = 'tags'
+
+    tag_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    tag_name = Column(VARCHAR(255))
+    tag_type = Column(Enum(TagType), default=TagType.USER)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    image_tag = relationship("ImageTag", back_populates="tags")
+
+
+
+# 이미지 태그 관계 테이블
+class ImageTag(Base):
+    __table__ = 'image_tag'
+
+    image_tag_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    tag_id = Column(Integer, ForeignKey("tags.tag_id"))
+    image_id = Column(Integer, ForeignKey("images.image_id"))
+
+    tags = relationship("Tags", back_populates="image_tag")
+    images = relationship("Images", back_populates="image_tag")
