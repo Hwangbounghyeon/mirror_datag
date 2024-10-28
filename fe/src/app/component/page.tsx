1d88d1463252cd229911af804963be81b2687657
SnowyPacks <originpc@naver.com>
2024-10-28 09:43:17 +0900

    [#10] Feature: 임시 컴포넌트 라우트 구축

--- fe/src/app/component/page.tsx
+++ fe/src/app/component/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import Sidebar from "@/components/common/sidebar";
 import React from "react";
 
 const Page = () => {
   return (
-    <section className="flex flex-row justify-center">
-      <div className="w-[30%]">
-        <Sidebar></Sidebar>
-      </div>
-
+    <section className="flex flex-col justify-center">
       <div className="flex flex-col justify-center align-middle items-center w-full">
         <h1 className="mt-3 text-[30px]">Component Page</h1>
 
