99d4710c88f6f1e3564e08e36b022a6a4b903327
황병현 <grant0808@naver.com>
2024-10-28 23:59:10 +0900

    [#12] feature : create image

--- DL/app/models/mariadb_image.py
+++ DL/app/models/mariadb_image.py
@@ -1,9 +1,8 @@
 from configs.mariadb import Base
-from sqlalchemy import Column, ForeignKey, Integer, VARCHAR, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, VARCHAR, DateTime, Enum as SAEnum
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from enum import Enum, unique
-
-
 
 @unique
 class TagType(Enum):
@@ -15,40 +14,36 @@ class TagType(Enum):
     EQUIPMENT = "EQUIPMENT"
     USER = "USER"
 
-
-
 # 이미지 테이블
 class Images(Base):
-    __table__ = "images"
+    __tablename__ = "images"
 
     image_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     metadata_id = Column(VARCHAR(255))
     feature_id = Column(VARCHAR(255))
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
 
     image_tag = relationship("ImageTag", back_populates="images")
     dataset_image = relationship("DatasetImage", back_populates="images")
 
 
-
 # 태그 테이블
 class Tags(Base):
-    __table__ = 'tags'
+    __tablename__ = 'tags'
 
     tag_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     tag_name = Column(VARCHAR(255))
-    tag_type = Column(Enum(TagType), default=TagType.USER)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
+    tag_type = Column(SAEnum(TagType), default=TagType.USER)  
+    created_at = Column(DateTime, default=func.now())  
+    updated_at = Column(DateTime, onupdate=func.now())  
 
     image_tag = relationship("ImageTag", back_populates="tags")
 
 
-
 # 이미지 태그 관계 테이블
 class ImageTag(Base):
-    __table__ = 'image_tag'
+    __tablename__ = 'image_tag'
 
     image_tag_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     tag_id = Column(Integer, ForeignKey("tags.tag_id"))
