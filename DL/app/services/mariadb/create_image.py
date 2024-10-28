5872abd45c8ab97a00f48cfeb6bd8240baf315c0
황병현 <grant0808@naver.com>
2024-10-28 17:48:53 +0900

    [#12] feature : mariadb table 설계

--- DL/app/services/mariadb/create_image.py
+++ DL/app/services/mariadb/create_image.py
@@ -1,5 +1,29 @@
 from fastapi import HTTPException
-from models.mariadb_image import Image
+from models.mariadb_image import Images, Tags, ImageTag
+from sqlalchemy.orm import Session
+from datetime import datetime, timezone
 
-async def create_image():
-    pass
+class CreateImage:
+    def __init__(self):
+        pass
+
+    async def create_image(self,object_idx: str, feature_idx: str, db: Session):
+        image = Images(
+            object_id = object_idx,
+            feature_id = feature_idx,
+            created_at = datetime.now(timezone.utc).isoformat(),
+            updated_at = datetime.now(timezone.utc).isoformat()
+        )
+
+        db.add(image)
+        db.commit()
+        db.refresh(image)
+    
+
+
+    async def create_tag(self, image_id: int, db: Session):
+        
+        image_tag = ImageTag
+        return 
+
+
