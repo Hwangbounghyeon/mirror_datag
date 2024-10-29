9792e7c5805543672af276baa51a35b99bece5d0
황병현 <grant0808@naver.com>
2024-10-29 17:49:26 +0900

    [#19] feature : batch 생성

--- BE/app/services/upload_service.py
+++ BE/app/services/upload_service.py
@@ -1,5 +1,7 @@
 from fastapi import  HTTPException
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+from datetime import datetime, timezone
+from sqlalchemy.orm import Session
 import mimetypes
 import zipfile
 import requests
@@ -8,6 +10,7 @@ import json
 
 
 
+from models.mariadb_users import UploadBatch
 from dto.uploads import UploadBase
 from configs.s3 import upload_to_s3
 
@@ -17,14 +20,29 @@ BUCKENAME = 'ssafy-project'
 
 
 class Upload:
-    def __init__(self):
-        pass
+    def __init__(self, db : Session):
+        self.db = db
 
     def is_image(self, filename : str):
         mime_type, _ = mimetypes.guess_type(filename)
         return mimetypes is not None and mime_type.startswith("image")
 
     async def upload_image(self, upload_base: UploadBase, files: list):
+
+        # 이미지 업로드 확인 Table 생성
+        batch_before = UploadBatch(
+            project_id = upload_base["project_id"],
+            user_id = upload_base["user_id"],
+            is_done = 0,
+            created_at = datetime.now(timezone.utc),
+            updated_at = datetime.now(timezone.utc)
+        )
+        
+        self.db.add(batch_before)
+        self.db.commit()
+        self.db.refresh(batch_before)
+
+
         file_urls = []
 
         for file in files:
@@ -79,5 +97,12 @@ class Upload:
         headers = {"Content-Type": "application/json"}
 
         requests.post(url, data = json_data, headers=headers)
+
+        batch_after = self.db.query(UploadBatch).filter(UploadBatch.upload_batch_id == batch_before.upload_batch_id)
+        if batch_after:
+            batch_after.is_done = 1
+            batch_after.updated_at = datetime.now(timezone.utc)
+            self.db.commit()
+
 
         return file_urls
