52d1c46a5e247122f81d7bc9f510bb17c26b47bd
SnowyPacks <originpc@naver.com>
2024-10-30 23:23:42 +0900

    [#31] Chore: 임시 border 표시

--- fe/src/app/(feature)/project/(home)/page.tsx
+++ fe/src/app/(feature)/project/(home)/page.tsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="w-full h-full px-2 py-2">
-      <header className="w-full flex flex-row justify-between align-middle items-center px-10 flex-wrap">
-        <h1 className="text-[30px] font-bold p-0">Projects</h1>
-        <button className="bg-blue-300 font-bold text-[24px] text-white border-none">
-          Create Project
-        </button>
-      </header>
+    <div className="w-full h-full flex flex-col border-2 border-red-300 px-2 py-2">
+      {/* 검색 영역 */}
+      <section>
+        <div className="flex flex-row items-center flex-wrap"></div>
+      </section>
     </div>
   );
 };
