a35ab7f5bfb94b706cbcae7f6cd25066622f2f91
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-10-30 13:13:27 +0900

    [#15] Style: Image Upload,Load UI 수정

--- fe/src/app/(feature)/upload/page.tsx
+++ fe/src/app/(feature)/upload/page.tsx
@@ -5,62 +5,32 @@ import { UploadContent } from "@/components/upload/uploadContent";
 import { useImageState } from "@/hooks/useImageState";
 import { useNavigation } from "@/hooks/useNavigation";
 import { useFileValidation } from "@/hooks/useFileValidation";
+import { PageHeader } from "@/components/common/pageHeader";
 
 export default function UploadPage() {
-  const { images, addImages, deleteImage, deleteAllImages } = useImageState();
+    const { images, addImages, deleteImage, deleteAllImages } = useImageState();
 
-  const { goBack, goToLoadImages } = useNavigation();
+    const { goBack, goToLoadImages } = useNavigation();
 
-  const { handleFileValidation } = useFileValidation({
-    images,
-    onValidFiles: addImages,
-  });
+    const { handleFileValidation } = useFileValidation({
+        images,
+        onValidFiles: addImages,
+    });
 
-  interface PageHeaderProps {
-    title: string;
-    onPrevious: () => void;
-    rightButtonText: string;
-    onRightButtonClick?: () => void;
-  }
-
-  const PageHeader: React.FC<PageHeaderProps> = ({
-    title,
-    onPrevious,
-    rightButtonText,
-    onRightButtonClick,
-  }) => (
-    <div className="flex justify-between items-center px-6 py-8 relative">
-      <button className="px-6 py-2" onClick={onPrevious}>
-        Previous
-      </button>
-
-      <h1 className="text-4xl absolute left-1/2 -translate-x-1/2 font-bold">
-        {title}
-      </h1>
-
-      <button
-        className="px-6 mx-2 py-2 rounded-lg"
-        onClick={onRightButtonClick}
-      >
-        {rightButtonText}
-      </button>
-    </div>
-  );
-
-  return (
-    <PageContainer>
-      <PageHeader
-        title="Upload Image"
-        rightButtonText="Move To Dataset"
-        onRightButtonClick={goToLoadImages}
-        onPrevious={goBack}
-      />
-      <UploadContent
-        images={images}
-        onFileUpload={handleFileValidation}
-        onDeleteImage={deleteImage}
-        onDeleteAllImages={deleteAllImages}
-      />
-    </PageContainer>
-  );
+    return (
+        <PageContainer>
+            <PageHeader
+                title="Upload Image"
+                rightButtonText="Move To Dataset"
+                onRightButtonClick={goToLoadImages}
+                onPrevious={goBack}
+            />
+            <UploadContent
+                images={images}
+                onFileUpload={handleFileValidation}
+                onDeleteImage={deleteImage}
+                onDeleteAllImages={deleteAllImages}
+            />
+        </PageContainer>
+    );
 }
