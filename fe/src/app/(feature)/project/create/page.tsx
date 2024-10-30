34c79de60e8f1718c02171f5fce71c22f1fa838e
SnowySparks <originpc@naver.com>
2024-10-30 14:21:10 +0900

    [#24] Fix : 애니메이션 제거 및 검색 부분 Fix.

--- fe/src/app/(feature)/project/create/page.tsx
+++ fe/src/app/(feature)/project/create/page.tsx
@@ -11,7 +11,6 @@ import Step4 from "@/components/project/create/step4";
 
 const Page = () => {
   const [step, setStep] = useState(1);
-  const [direction, setDirection] = useState(0); // -1: 왼쪽, 1: 오른쪽
 
   const [projectItem, setProjectItem] = useState<ProjectType>({
     category: null,
@@ -23,33 +22,9 @@ const Page = () => {
 
   const handleMove = (stepNumber: number) => {
     // 이동 방향 설정
-    setDirection(stepNumber > step ? 1 : -1);
-
     if (stepNumber < 1) setStep(1);
     else if (stepNumber > 4) setStep(4);
     else setStep(stepNumber);
-  };
-
-  // 슬라이드 애니메이션 variants
-  const variants = {
-    enter: (direction: number) => ({
-      x: direction > 0 ? 1000 : -1000,
-      opacity: 0,
-    }),
-    center: {
-      x: 0,
-      opacity: 1,
-    },
-    exit: (direction: number) => ({
-      x: direction > 0 ? -1000 : 1000,
-      opacity: 0,
-    }),
-  };
-
-  // 애니메이션 전환 설정
-  const transition = {
-    duration: 0.3,
-    type: "tween",
   };
 
   return (
@@ -62,41 +37,29 @@ const Page = () => {
           </div>
         </header>
 
-        <div className="flex justify-center items-center overflow-x-hidden overflow-y-visible">
-          <AnimatePresence initial={false} mode="wait" custom={direction}>
-            <motion.div
-              key={step}
-              custom={direction}
-              variants={variants}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={transition}
-            >
-              {step === 1 && (
-                <Step1
-                  projectItem={projectItem}
-                  setProjectItem={setProjectItem}
-                  handleMove={handleMove}
-                />
-              )}
-              {step === 2 && (
-                <Step2
-                  projectItem={projectItem}
-                  setProjectItem={setProjectItem}
-                  handleMove={handleMove}
-                />
-              )}
-              {step === 3 && (
-                <Step3
-                  projectItem={projectItem}
-                  setProjectItem={setProjectItem}
-                  handleMove={handleMove}
-                />
-              )}
-              {step === 4 && <Step4 />}
-            </motion.div>
-          </AnimatePresence>
+        <div className="w-full flex justify-center items-center">
+          {step === 1 && (
+            <Step1
+              projectItem={projectItem}
+              setProjectItem={setProjectItem}
+              handleMove={handleMove}
+            />
+          )}
+          {step === 2 && (
+            <Step2
+              projectItem={projectItem}
+              setProjectItem={setProjectItem}
+              handleMove={handleMove}
+            />
+          )}
+          {step === 3 && (
+            <Step3
+              projectItem={projectItem}
+              setProjectItem={setProjectItem}
+              handleMove={handleMove}
+            />
+          )}
+          {step === 4 && <Step4 />}
         </div>
       </div>
     </div>
