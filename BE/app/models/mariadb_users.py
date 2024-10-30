8fe23bcfa5e5dc8e1c9fb63de95d6a8570c70843
김호경 <ssafy1123992@gmail.com>
2024-10-30 13:39:25 +0900

    [#27] user 기능 병합을 위한 코드 수정

--- BE/app/models/mariadb_users.py
+++ BE/app/models/mariadb_users.py
@@ -1,30 +1,12 @@
+from datetime import datetime
 from configs.mariadb import Base
 from models.mariadb_image import Images
 
-from sqlalchemy import Column, ForeignKey, Integer, VARCHAR, DateTime, TEXT
+from sqlalchemy import (
+    Column, ForeignKey, Integer, VARCHAR, DateTime, TEXT, Boolean, String, TIMESTAMP
+)
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-
-
-
-class Users(Base):
-    __tablename__ = "users"
-
-    user_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(VARCHAR(50), nullable=False)
-    email = Column(VARCHAR(255), nullable=False, unique=True)
-    password = Column(VARCHAR(255), nullable=False)
-    duty = Column(VARCHAR(255), nullable=False)
-    location = Column(VARCHAR(255), nullable=False)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now())
-    department = Column(Integer,ForeignKey("departments.department_id"), nullable=True)
-
-    departments = relationship("Departments", back_populates="users")
-    projects = relationship("Project", back_populates="users")
-    histories = relationship("Histories", back_populates="users")
-    upload_batch = relationship("UploadBatch", back_populates="users")
-
 
 
 class Departments(Base):
@@ -35,25 +17,44 @@ class Departments(Base):
 
     users = relationship("Users", back_populates="departments")
 
+class Users(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(VARCHAR(255), nullable=False)
+    email = Column(VARCHAR(255), nullable=False)
+    password = Column(VARCHAR(255), nullable=False)
+    duty = Column(VARCHAR(255), nullable=False)
+    location = Column(VARCHAR(255), nullable=False)
+    department = Column(Integer, ForeignKey('departments.department_id', ondelete="SET NULL"), nullable=True)
+    is_supervised = Column(Boolean, nullable=False)
+    created_at = Column(TIMESTAMP, default=datetime.now(), nullable=False)
+    updated_at = Column(TIMESTAMP, default=datetime.now(), onupdate=datetime.now(), nullable=False)
+
+    departments = relationship("Departments", back_populates="users")
+    projects = relationship("Projects", back_populates="users")
+    histories = relationship("Histories", back_populates="users")
+    upload_batches = relationship("UploadBatches", back_populates="users")
 
 
-class Project(Base):
-    __tablename__ = "project"
 
-    project_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=True)
-    permission_id = Column(VARCHAR(255), nullable=False)
-    is_private = Column(Integer, default=0)
+class Projects(Base):
+    __tablename__ = "projects"
+
+    project_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="SET NULL"), nullable=True)
+    permission_id = Column(VARCHAR(255), nullable=False) # MongoDB reperence
+    is_private = Column(Boolean, nullable=False, default=0)
     project_name = Column(VARCHAR(255), nullable=False)
     model_name = Column(VARCHAR(255), nullable=False)
     description = Column(TEXT, nullable=True)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now())
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, onupdate=func.now(), nullable=False)
 
     users = relationship("Users", back_populates="projects")
     project_image = relationship("ProjectImage", back_populates="projects")
     histories = relationship("Histories", back_populates="projects")
-    upload_batch = relationship("UploadBatch", back_populates="projects")
+    upload_batches = relationship("UploadBatches", back_populates="projects")
 
 
 
@@ -61,10 +62,10 @@ class ProjectImage(Base):
     __tablename__ = "project_image"
 
     project_image = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    project_id = Column(Integer, ForeignKey("project.project_id"))
-    image_id = Column(Integer, ForeignKey("images.image_id"))
+    project_id = Column(Integer, ForeignKey("project.project_id", ondelete="CASCADE"), nullable=False)
+    image_id = Column(Integer, ForeignKey("images.image_id", ondelete="CASCADE"), nullable=False)
 
-    projects = relationship("Project", back_populates="project_image")
+    projects = relationship("Projects", back_populates="project_image")
     images = relationship("Images", back_populates="project_image")
 
 
@@ -73,28 +74,28 @@ class Histories(Base):
     __tablename__ = "histories"
 
     history_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("users.user_id"))
-    project_id = Column(Integer, ForeignKey("project.project_id"))
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="SET NULL"))
+    project_id = Column(Integer, ForeignKey("project.project_id", ondelete="CASCADE"), nullable=False)
     history_name = Column(VARCHAR(255), nullable=False)
-    history_obj_id = Column(VARCHAR(255))
-    is_private = Column(Integer, nullable=False)
-    is_done = Column(Integer, nullable=False, default=0)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now())
+    history_obj_id = Column(VARCHAR(255)) # MongoDB reference
+    is_private = Column(Boolean, nullable=False)
+    is_done = Column(Boolean, nullable=False, default=0)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, onupdate=func.now(), nullable=False)
 
     users = relationship("Users", back_populates="histories")
-    projects = relationship("Project", back_populates="histories")
+    projects = relationship("Projects", back_populates="histories")
 
-class UploadBatch(Base):
-    __tablename__ = "upload_batch"
+class UploadBatches(Base):
+    __tablename__ = "upload_batches"
     upload_batch_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    project_id = Column(Integer, ForeignKey("project.project_id"), nullable=False)
-    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=True)
+    project_id = Column(Integer, ForeignKey("project.project_id", ondelete="CASCADE"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="SET NULL"))
     is_done = Column(Integer, nullable=False, default=0)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now())
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, onupdate=func.now(), nullable=False)
 
-    projects = relationship("Project", back_populates="upload_batch")
-    users = relationship("Users", back_populates="upload_batch")
+    projects = relationship("Projects", back_populates="upload_batches")
+    users = relationship("Users", back_populates="upload_batches")
 
     
