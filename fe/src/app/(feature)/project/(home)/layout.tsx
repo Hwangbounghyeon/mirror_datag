abd09c598f24c3a73e088e9c9daa47a83fb04ecd
SnowySparks <originpc@naver.com>
2024-10-30 16:03:30 +0900

    [#31] Chore: Layout 수정

--- fe/src/app/(feature)/project/(home)/layout.tsx
+++ fe/src/app/(feature)/project/(home)/layout.tsx
@@ -1,25 +1,24 @@
 import React from "react";
+import Link from "next/link";
 
 interface LayoutProps {
-  project_list: React.ReactNode;
   children: React.ReactNode;
 }
 
-const Layout = ({ project_list }: LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="w-full h-full px-2 py-2">
+    <div className="w-full h-full flex flex-col items-center px-2 py-2">
       <header className="w-full flex flex-row justify-between align-middle items-center px-10 flex-wrap">
         <h1 className="text-[30px] font-bold p-0">Projects</h1>
-        <button className="bg-blue-300 font-bold text-[24px] text-white border-none">
+
+        <Link
+          href={"create"}
+          className="bg-blue-300 font-bold text-[24px] text-white border-none"
+        >
           Create Project
-        </button>
+        </Link>
       </header>
-      <div className="flex flex-row flex-wrap">
-        <div className="flex flex-row">
-          <p>Department</p>
-          <select></select>
-        </div>
-      </div>
+      <div className="flex-1">{children}</div>
     </div>
   );
 };
