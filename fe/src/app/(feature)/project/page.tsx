bba72238365ae750b16d0ab9d726480a7ac3ba7b
SnowySparks <originpc@naver.com>
2024-10-30 15:10:37 +0900

    [#31] Feature: Home Page 헤더 구축

--- fe/src/app/(feature)/project/page.tsx
+++ fe/src/app/(feature)/project/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Project</div>;
+  return (
+    <div className="w-full h-full px-2 py-2">
+      <header className="w-full flex flex-row justify-between align-middle items-center px-10 flex-wrap">
+        <h1 className="text-[30px] font-bold p-0">Projects</h1>
+        <button className="bg-blue-300 font-bold text-[24px] text-white border-none">
+          Create Project
+        </button>
+      </header>
+    </div>
+  );
 };
 
 export default Page;
