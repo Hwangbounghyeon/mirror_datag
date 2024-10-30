75b5a927e558e58134c178db9fe99a5208748e78
SnowySparks <originpc@naver.com>
2024-10-30 14:57:12 +0900

    [#31] Fix : Project 생성용 타입 변경 및 실제 ProjectType 정의

--- fe/src/types/projectType.ts
+++ fe/src/types/projectType.ts
@@ -1,16 +1,29 @@
 export type categoryType = "object-detection" | "image-classification" | null;
 export type modelType = "yolo_v5" | "yolo_v8" | "yolo_v11" | null;
 
-export type ProjectType = {
+export type CreateProjectType = {
   category: categoryType;
-  model: modelType;
-  name: string; // Project name
+  model_name: modelType;
+  project_name: string; // Project name
   description: string; // Project description
   additional_permission: number[]; // 부서 추가 권한
+  is_private: boolean; // 비공개 여부
+};
+
+export type PojectType = {
+  project_id: number;
+  user_id: number;
+  project_name: string;
+  description: string;
+  model_name: string;
+  is_private: boolean;
+  permission_id: string;
+  created_at: string;
+  updated_at: string;
 };
 
 export interface StepProps {
   handleMove?: (stepNumber: number) => void;
-  projectItem: ProjectType;
-  setProjectItem: React.Dispatch<React.SetStateAction<ProjectType>>;
+  projectItem: CreateProjectType;
+  setProjectItem: React.Dispatch<React.SetStateAction<CreateProjectType>>;
 }
