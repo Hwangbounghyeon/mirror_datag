5575be42da3bb9e10c4302f7dead6b7974be0e50
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-10-31 17:37:30 +0900

    [#32] Style: 스타일 변경

--- fe/src/components/imagedeatil/metadataPanel.tsx
+++ fe/src/components/imagedeatil/metadataPanel.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function MetadataPanel() {
-    return <div className="flex flex-col gap-2 px-4">MetadataPanel</div>;
+    return (
+        <div className="flex flex-col px-4">
+            MetadataPanel
+        </div>
+    );
 }
 
 export default MetadataPanel;
