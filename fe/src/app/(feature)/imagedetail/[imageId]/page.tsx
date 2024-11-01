e57113c427bdabaf564248cb01ca97abeee1fa31
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-11-01 17:39:08 +0900

    [#15] Style: 색상 값 변경

--- fe/src/app/(feature)/imagedetail/[imageId]/page.tsx
+++ fe/src/app/(feature)/imagedetail/[imageId]/page.tsx
@@ -81,7 +81,7 @@ function ImageDetailPage() {
             <div className="flex flex-1 overflow-hidden">
                 <div className="w-[20%]">
                     <div className="flex flex-col h-full">
-                        <div className="h-1/3 border-b border-gray-700">
+                        <div className="h-1/3 border-b border-gray-700 dark:border-gray-50">
                             <div className="flex px-2 py-2 pb-2 bg-gray-50 dark:bg-gray-500">
                                 <div
                                     className={`cursor-pointer px-4 py-1 ${
@@ -111,7 +111,7 @@ function ImageDetailPage() {
                                 <MetadataPanel />
                             )}
                         </div>
-                        <div className="h-1/3 border-b border-gray-700">
+                        <div className="h-1/3 border-b border-gray-700 dark:border-gray-50">
                             <AuthPanel
                                 authorities={authorities}
                                 onRemove={removeAuthority}
