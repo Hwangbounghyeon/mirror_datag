18353778421b997b85b57335c1e8784ba43e14ff
SnowySparks <originpc@naver.com>
2024-11-01 10:11:03 +0900

    [#37] Feature: UI 변경 및 Sidebar 버튼추가. Project URL 연결

--- fe/src/components/common/sidebar-item.tsx
+++ fe/src/components/common/sidebar-item.tsx
@@ -10,8 +10,8 @@ interface SidebarItemProps {
 const SidebarItem = ({ title, Icon, link }: SidebarItemProps) => {
   return (
     <Link href={link || "/"}>
-      <div className="w-full mt-1 mb-1 h-[50px] border-blue-300 border-b-2 border-b-slate-300 dark:border-b-slate-300 flex flex-row items-center transition-all hover:scale-105 active:scale-95">
-        <Icon size={30} />
+      <div className="w-full mt-1 mb-1 h-[50px] border-blue-300 border-b-2 border-b-slate-300 dark:border-b-slate-300 flex flex-row items-center transition-all hover:scale-[102%] active:scale-[98%]">
+        <Icon size={24} />
         <div className="mx-3 text-[20px]">{title}</div>
       </div>
     </Link>
