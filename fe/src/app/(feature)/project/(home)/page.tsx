93e84a1712a6b719e02e920ebbf6bd4df9978217
SnowySparks <originpc@naver.com>
2024-11-01 10:31:31 +0900

    HOTFIX: 용어 오타 Fix

--- fe/src/app/(feature)/project/(home)/page.tsx
+++ fe/src/app/(feature)/project/(home)/page.tsx
@@ -9,7 +9,7 @@ interface PageProps {
   };
 }
 
-const ProjecdtList = async () => {
+const ProjectList = async () => {
   const projects = await getProjects();
   return (
     <div className="w-full flex flex-col items-center flex-grow mt-5">
@@ -24,7 +24,7 @@ const Page = ({ searchParams }: PageProps) => {
   return (
     <div className="overflow-y-scroll bg-slate-100 dark:bg-gray-700 py-2 px-2 my-3 rounded-md w-full h-full flex flex-col items-center flex-grow remove_scrollbar">
       <Suspense fallback={<div>Loading...</div>}>
-        <ProjecdtList />
+        <ProjectList />
       </Suspense>
     </div>
   );
