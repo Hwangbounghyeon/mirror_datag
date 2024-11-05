a3737e22b17b64695d2cde53454da1a3c9da76a8
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-11-05 15:54:52 +0900

    [#51] Fix: tag와 department id 삭제

--- fe/src/types/auth.ts
+++ fe/src/types/auth.ts
@@ -1,21 +1,11 @@
-export interface Department {
-    department_id: number;
-    department_name: string;
-}
-
 export interface User {
     user_id: number;
     name: string;
-    department: number;
+    department_name: string;
 }
 
 export interface Authority {
     id: number;
     name: string;
     department: string;
-}
-
-export interface Tag {
-    id: number;
-    tag: string;
 }
