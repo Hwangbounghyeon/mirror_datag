b7538681f1a23cb2b07b7738284b9c5e28756a1a
SnowySparks <originpc@naver.com>
2024-11-05 11:04:47 +0900

    [#39] Feature: Server Action 함수 및 타입 정의

--- fe/src/lib/axiosApi.ts
+++ fe/src/lib/axiosApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { store } from "@/store/store";
+import store from "@/store/store";
 import { clearAuth, setAccessToken } from "@/store/authSlice";
 
 const api = axios.create({
