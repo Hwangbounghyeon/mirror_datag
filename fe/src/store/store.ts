7e6f12c26c196a8cbd8e8a5908e54ce5708e8808
SnowySparks <originpc@naver.com>
2024-11-05 09:58:56 +0900

    [#39] Feature: Redux 설정

--- fe/src/store/store.ts
+++ fe/src/store/store.ts
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import userInfoReducer from "./userInfoSlice";
 import authReducer from "./authSlice";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     userInfo: userInfoReducer,
     auth: authReducer,
   },
 });
+
+export default store;
