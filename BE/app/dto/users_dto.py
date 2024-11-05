a7efcddc68319cdc6a6a43854ed7d1e9f699c5f5
김호경 <ssafy1123992@gmail.com>
2024-11-05 17:52:33 +0900

    [#54] JWT 토큰 보안 강화를 위한 기능 변경

--- BE/app/dto/users_dto.py
+++ BE/app/dto/users_dto.py
@@ -1,15 +1,8 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr
 
 class DepartmentDTO(BaseModel):
     department_id: int
     department_name: str
-
-    # @field_validator('department_name')
-    # def not_empty(cls, v):
-    #     if not v or not v.strip():
-    #         raise ValueError('"departments" 테이블, "department_name"에 유효한 값을 입력해 주세요. 현재 값: "{v}"')
-    #     return v
-
     class Config:
         from_attributes = True
 
@@ -22,13 +15,6 @@ class UserCreateDTO(BaseModel):
     location: str
     department_id: int | None = None
     is_supervised: bool
-
-    # @field_validator('email', 'password', 'duty', 'location', 'is_supervised')
-    # def not_empty(cls, v):
-    #     if v is None or (isinstance(v, str) and not v.strip()):
-    #         raise ValueError('"users"테이블, f"{field.name}"에 유효한 값을 입력해 주세요. 현재 값: "{v}"')
-    #     return v
-
     class Config:
         from_attributes = True
         
