4edaa11771b1f57484c11a0499a38865200bb4e5
최현성 <huns1013@gmail.com>
2024-11-06 12:37:19 +0900

    [#38] Feature: analysis

--- fe/src/app/(feature)/project/[project_id]/analysis/page.tsx
+++ fe/src/app/(feature)/project/[project_id]/analysis/page.tsx
@@ -1,28 +1,21 @@
-import React, { Suspense, useState } from "react";
-import { SideHistories } from "@/components/project/analysis/side-histories";
-import { HistoryList } from "@/components/project/analysis/history-list";
+"use client";
+import React, { useState, useEffect } from "react";
+import SideHistories from "@/components/project/analysis/side-histories";
 import MainAnalysis from "@/components/project/analysis/main-analysis";
 
 import { HistoryListData } from "@/types/historyType";
 
 const Page = ({ params }: { params: { project_id: string } }) => {
+  const [selectedHistory, setSelectedHistory] = useState<string | null>(null);
+
   return (
     <div className="h-full flex flex-col gap-[1rem] mx-[3rem] pt-[2rem]">
       <div className="w-full h-[3rem] flex justify-between">
         <div className="text-3xl">Analysis</div>
       </div>
       <div className="w-full flex flex-grow">
-        <SideHistories projectId={params.project_id}>
-          <Suspense fallback={
-            <div className="flex-grow bg-red-500 overflow-auto flex items-center justify-center">
-              Loading histories...
-            </div>
-          }>
-            {/* <HistoryList projectId={params.project_id} page={page} limit={limit}/> */}
-            <HistoryList projectId={params.project_id}/>
-          </Suspense>
-        </SideHistories>
-        <MainAnalysis />
+        <SideHistories projectId={params.project_id} setSelectedHistory={setSelectedHistory}/>
+        <MainAnalysis selectedHistory={selectedHistory}/>
       </div>
     </div>
   );
