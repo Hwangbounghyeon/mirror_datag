c309f4901860a6b6f8529595550c16f6d9395eda
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-11-06 10:59:23 +0900

    [#51] Feature: S3이미지 보여주기 위한 권한 추가

--- fe/next.config.mjs
+++ fe/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["picsum.photos"],
-  },
+    images: {
+        domains: ["picsum.photos", "ssafy-project.s3.us-east-2.amazonaws.com"],
+    },
 };
 
 export default nextConfig;
