b0a1fdce669d575ec983ef74a751a3a97c944d86
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-11-06 17:26:56 +0900

    [#51] Style: 색상 값 변경

--- fe/src/components/imagedeatil/header.tsx
+++ fe/src/components/imagedeatil/header.tsx
@@ -21,13 +21,13 @@ function Header({
     const isLastPage = currentNumber === totalCount;
 
     return (
-        <div className="w-full flex items-center relative pb-2 bg-gray-50 dark:bg-gray-500">
-            <div
-                className="flex flex-col px-2 cursor-pointer"
-                onClick={() => router.push("/dataset")}
-            >
+        <div className="w-full flex items-center relative pt-1 pb-2 bg-gray-50 dark:bg-gray-500">
+            <div className="flex flex-col px-2">
                 <p className="pb-1">
-                    <FaArrowLeftLong />
+                    <FaArrowLeftLong
+                        className="cursor-pointer"
+                        onClick={() => router.push("/dataset")}
+                    />
                 </p>
                 <span>{fileName}</span>
             </div>
