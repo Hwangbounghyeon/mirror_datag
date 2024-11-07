aa10e576c5abfe396b728e6378386264996ba8b8
김호경 <ssafy1123992@gmail.com>
2024-11-07 18:06:06 +0900

    [#71] Profile 조회, 수정 기능 추가

--- BE/app/dto/users_dto.py
+++ BE/app/dto/users_dto.py
@@ -24,18 +24,6 @@ class UserSignIn(BaseModel):
     
     class Config:
         from_attributes = True
-
-class UserInfoResponse(BaseModel):
-    user_id: int
-    name: str
-    email: str
-    duty: str
-    location: str
-    department_id: int | None = None
-    is_supervised: bool
-    
-    class Config:
-        from_attributes = True
         
 class TokenResponse(BaseModel):
     access_token: str
@@ -43,4 +31,30 @@ class TokenResponse(BaseModel):
     token_type: str = 'bearer'
     
     class Confing:
+        from_attributes = True
+        
+class UserProfileResponse(BaseModel):
+    user_id: int
+    name: str
+    email: str
+    duty: str
+    location: str
+    department_id: int | None = None
+    department_name: str | None = None
+    is_supervised: bool
+    created_at: datetime
+    updated_at: datetime
+    
+    class Config:
+        from_attributes = True
+        
+class UserProfileUpdateRequest(BaseModel):
+    name: str | None = None
+    duty: str | None = None
+    department_id: int | None = None
+    location: str | None = None
+    current_password: str | None = None
+    new_password: str | None = None
+    
+    class Config:
         from_attributes = True
