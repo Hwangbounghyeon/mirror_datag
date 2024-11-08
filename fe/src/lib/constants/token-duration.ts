f0aa7744377c25f376caa6172961c7c8c18bdf08
SnowySparks <originpc@naver.com>
2024-11-08 12:48:21 +0900

    [#75] Chore: AccessToken , RefreshToken 유효시간 변경

--- fe/src/lib/constants/token-duration.ts
+++ fe/src/lib/constants/token-duration.ts
@@ -1,2 +1,2 @@
-export const accessTokenDuration: number = 60 * 30;
-export const refreshTokenDuration: number = 60 * 60 * 6;
+export const accessTokenDuration: number = 60 * 10; // 10분
+export const refreshTokenDuration: number = 60 * 60 * 24 * 6; // 6
