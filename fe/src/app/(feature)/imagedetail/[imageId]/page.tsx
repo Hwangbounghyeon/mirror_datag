74c7293331bba6b0f74ced60419968468af287ab
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-11-10 19:14:25 +0900

    [#51] Feature: 이미지 상세화면 suspense 적용

--- fe/src/app/(feature)/imagedetail/[imageId]/page.tsx
+++ fe/src/app/(feature)/imagedetail/[imageId]/page.tsx
@@ -1,48 +1,14 @@
-import { loadImageDetail } from "@/api/detail/loadImageDetail";
 import { Suspense } from "react";
-import ImageDetailClient from "./imageDetailClient";
+import { ImageDetailContent } from "./ImageDetailContent";
 
 export default async function ImageDetailPage({
     params,
 }: {
     params: { imageId: string };
 }) {
-    const imageId = params.imageId;
-    const data = await loadImageDetail("672c4ad2d00bbc3d9b3d5d66"); //TODO 추후 수정
-
-    if (!data.data) {
-        throw new Error("Data is undefined");
-    }
-
-    const initialAuthorities = data.data.access_control.users.map((user) => ({
-        user_id: user.uid,
-        user_name: user.name,
-        department_name: user.department_name,
-    }));
-
-    const initialTags =
-        data.data.metadata.aiResults[0]?.predictions[0]?.tags || [];
-
-    const classes =
-        data.data.metadata.aiResults[0]?.predictions[0]?.detections.map(
-            (detection) => detection.prediction
-        ) || [];
-
-    const metadata = data.data.metadata.metadata;
-
-    const imageSrc = data.data.metadata.fileList[0];
-
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <ImageDetailClient
-                imageId={imageId}
-                imageIdx={parseInt(imageId, 10)} //TODO 추후 수정
-                initialAuthorities={initialAuthorities}
-                initialTags={initialTags}
-                classes={classes}
-                imageSrc={imageSrc}
-                metadata={metadata}
-            />
+        <Suspense fallback={<div>Loading image details...</div>}>
+            <ImageDetailContent params={params} />
         </Suspense>
     );
 }
