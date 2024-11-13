c5ca21b875a39bebe68d1c95db908131d02bc4f0
정재훈 <a58779@naver.com>
2024-11-13 17:54:26 +0900

    [#93] feature test code write

--- BE/tests/routers/test_project.py
+++ BE/tests/routers/test_project.py
@@ -1,9 +1,18 @@
 import pytest
+import json
+import sys
+import os
 
 from fastapi.testclient import TestClient
+from fastapi import HTTPException
 
 from app.main import app
 from app.configs.mariadb import get_database_mariadb
+from app.configs.mongodb import client as mongo_client
+
+from unittest.mock import Mock, patch
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'app')))
 
 client = TestClient(app)
 
@@ -65,3 +74,33 @@ def test_search_user_name():
     assert response.status_code == 200
     assert response.json()["status"] == 200
     assert isinstance(response.json()["data"], list)
+
+# 6. 프로젝트에 필터링된 이미지 저장
+def test_filter_image_add(auth_headers, mock_upload_service, mock_db):
+    add_filtering_image_request = {
+    "project_id": "6732f477fcec9d2c66a7507c",
+    "conditions": ["and_condition"]
+    }
+    files = [
+        ("files", ("test1.jpg", b"file_content_1", "image/jpeg")),
+        ("files", ("test2.jpg", b"file_content_2", "image/jpeg"))
+    ]
+    session = mongo_client.start_session()
+    session.start_transaction()
+
+    response = client.post(
+        "be/api/project/filterImage/{project_id}/list",
+        data={"add_filtering_image_request": json.dumps(add_filtering_image_request)},
+        files=files,
+        headers=auth_headers
+    )
+
+    session.abort_transaction()
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": 200,
+        "data": "이미지 업로드에 성공하였습니다."
+    }
+
+    mock_upload_service.return_value.upload_image.assert_called_once()
