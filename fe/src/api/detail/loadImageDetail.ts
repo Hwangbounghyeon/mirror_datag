092dbe646121cc5eb9323e407ddffa6c09d53a8a
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-11-14 15:40:45 +0900

    [#51] Feature: 이미지 로드 api 주소 및 request 수정

--- fe/src/api/detail/loadImageDetail.ts
+++ fe/src/api/detail/loadImageDetail.ts
@@ -1,13 +1,17 @@
 import { ImageDetailResponse } from "@/types/metadata";
 import apiClient from "../client";
+import { TagBySearchRequest } from "@/types/tag";
 
 export const loadImageDetail = async (
-    imageId: string
+    filterConditions: TagBySearchRequest,
+    imageId: string,
+    project_id: string
 ): Promise<ImageDetailResponse> => {
     const response = await apiClient<ImageDetailResponse>(
-        `/image/detail/${imageId}`,
+        `/image/detail/${project_id}/${imageId}`,
         {
-            method: "GET",
+            method: "POST",
+            body: JSON.stringify(filterConditions),
             cache: "no-store",
         }
     );
