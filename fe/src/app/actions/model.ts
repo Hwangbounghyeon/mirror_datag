ff47edc87e208a875dc2d4045ced00dce9dfee77
SnowySparks <originpc@naver.com>
2024-11-15 10:48:01 +0900

    [#99] Feature: Model 선택에 따른 검색 및 페이지네이션 구축 및 테스트완료

--- fe/src/app/actions/model.ts
+++ fe/src/app/actions/model.ts
@@ -1,38 +1,24 @@
 "use server";
 
-import { DefaultResponseType } from "./customFetch";
 import { ModelListResponseType } from "@/types/modelType";
+import { customFetch } from "./customFetch";
 
 export const getModels = async () => {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/project/model/list`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        next: {
-          tags: ["Models List"],
-        },
-      }
-    );
+    const response = await customFetch<ModelListResponseType>({
+      endpoint: "/project/model/list",
+      method: "GET",
+      cache: "no-store",
+    });
 
-    if (response.ok) {
-      const data: DefaultResponseType<ModelListResponseType> =
-        await response.json();
-      return data;
+    if (response.data) {
+      return response.data;
     } else {
-      return {
-        error: "부서를 불러오는 중 오류가 발생했습니다.",
-        status: response.status,
-      };
+      console.error(response.error);
+      return {};
     }
   } catch (error) {
-    console.error("부서 목록 가져오기", error);
-    return {
-      error: (error as string) || "부서를 불러오는 중 오류가 발생했습니다.",
-      status: 500,
-    };
+    console.error(error);
+    return {};
   }
 };
