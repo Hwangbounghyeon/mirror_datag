a92a041772335015894538dab39c7095bc0a0672
황병현 <grant0808@naver.com>
2024-11-15 16:53:10 +0900

    [#106] test : test folder 수정
    Former-commit-id: 08c3c4f016f81e985dbe611d6cda9483b512e55f

--- BE/app/services/project/upload_service.py
+++ BE/app/services/project/upload_service.py
@@ -181,11 +181,9 @@ class UploadService:
         department_id: int
     ):
         if task == "cls":
-            # url = f"http://{os.getenv('REDIS_HOST')}:8001/dl/api/cls"
-            url = "http://127.0.0.1:8001/dl/api/cls"
+            url = f"http://{os.getenv('REDIS_HOST')}:8001/dl/api/cls"
         else:
-            # url = f"http://{os.getenv('REDIS_HOST')}:8001/dl/api/det"
-            url = "http://127.0.0.1:8001/dl/api/det"
+            url = f"http://{os.getenv('REDIS_HOST')}:8001/dl/api/det"
 
 
         if department_id:
