010a26a227d1f3de85c07c0e83a50bf9be76f5c9
SnowyPacks <originpc@naver.com>
2024-11-17 22:36:40 +0900

    [#109] Chore: 통계부분에 대한 색 보정

--- fe/src/components/project/analysis/label-bar-chart.tsx
+++ fe/src/components/project/analysis/label-bar-chart.tsx
@@ -1,7 +1,7 @@
 // BarChart.tsx
-'use client'
+"use client";
 
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,12 +10,17 @@ import {
   Title,
   Tooltip,
   Legend,
-  ChartOptions
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+  ChartOptions,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 import { Card } from "@nextui-org/card";
-import { HistoryData, ObjectDetectionLabels, ReductionResults } from '@/types/historyType';
-import { LABEL_COLORS } from '@/lib/constants/labelColors';
+import {
+  HistoryData,
+  ObjectDetectionLabels,
+  ReductionResults,
+} from "@/types/historyType";
+import { LABEL_COLORS } from "@/lib/constants/labelColors";
+import { useTheme } from "next-themes";
 
 ChartJS.register(
   CategoryScale,
@@ -28,25 +33,36 @@ ChartJS.register(
 
 interface BarChartProps {
   data: HistoryData;
-  selectedLabels: string[]
+  selectedLabels: string[];
   setSelectedLabels: (s: string[]) => void;
-  displayMode: 'prediction' | 'real';
+  displayMode: "prediction" | "real";
   onLabelsSelect?: (points: ReductionResults[]) => void;
 }
 
-const BarChart = ({ data, selectedLabels, setSelectedLabels, displayMode, onLabelsSelect }: BarChartProps) => {
+const BarChart = ({
+  data,
+  selectedLabels,
+  setSelectedLabels,
+  displayMode,
+  onLabelsSelect,
+}: BarChartProps) => {
   // const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
-
+  const { theme } = useTheme();
   const isObjectDetectionLabels = (
     label: ObjectDetectionLabels | string | undefined
   ): label is ObjectDetectionLabels => {
-    return typeof label === 'object' && label !== null && 'label' in label && 'bbox' in label;
+    return (
+      typeof label === "object" &&
+      label !== null &&
+      "label" in label &&
+      "bbox" in label
+    );
   };
 
   const distributionData = useMemo(() => {
     const distribution: { [key: string]: number } = {};
-    
-    data.results?.forEach(result => {
+
+    data.results?.forEach((result) => {
       if (result.predictions?.prediction) {
         const label = result.predictions.prediction;
         distribution[label] = (distribution[label] || 0) + 1;
@@ -60,29 +76,29 @@ const BarChart = ({ data, selectedLabels, setSelectedLabels, displayMode, onLabe
     labels: Object.keys(distributionData),
     datasets: [
       {
-        label: 'Label Distribution',
+        label: "Label Distribution",
         data: Object.values(distributionData),
         backgroundColor: Object.keys(distributionData).map(
-          label => LABEL_COLORS[label] || 'rgba(0, 0, 0, 0.6)'
+          (label) => LABEL_COLORS[label] || "rgba(0, 0, 0, 0.6)"
         ),
-        borderWidth: Object.keys(distributionData).map(
-          label => selectedLabels.includes(label) ? 5 : 1
+        borderWidth: Object.keys(distributionData).map((label) =>
+          selectedLabels.includes(label) ? 5 : 1
         ),
-        borderColor: Object.keys(distributionData).map(
-          label => selectedLabels.includes(label) 
-            ? '#FF4500'
-            : LABEL_COLORS[label] || 'rgba(0, 0, 0, 0.6)'
+        borderColor: Object.keys(distributionData).map((label) =>
+          selectedLabels.includes(label)
+            ? "#FF4500"
+            : LABEL_COLORS[label] || "rgba(0, 0, 0, 0.6)"
         ),
-        hoverBackgroundColor: Object.keys(distributionData).map(
-          label => selectedLabels.includes(label) 
-            ? LABEL_COLORS[label] 
-            : 'rgba(0, 0, 0, 0.6)'
-        )
+        hoverBackgroundColor: Object.keys(distributionData).map((label) =>
+          selectedLabels.includes(label)
+            ? LABEL_COLORS[label]
+            : "rgba(0, 0, 0, 0.6)"
+        ),
       },
     ],
   };
 
-  const options: ChartOptions<'bar'> = {
+  const options: ChartOptions<"bar"> = {
     responsive: true,
     maintainAspectRatio: false,
     plugins: {
@@ -90,14 +106,14 @@ const BarChart = ({ data, selectedLabels, setSelectedLabels, displayMode, onLabe
         display: false,
       },
       tooltip: {
-        backgroundColor: 'rgba(255, 255, 255, 0.95)',
-        titleColor: '#334155',
-        bodyColor: '#334155',
+        backgroundColor: "rgba(255, 255, 255, 0.95)",
+        titleColor: "#334155",
+        bodyColor: "#334155",
         bodyFont: {
-          size: 12
+          size: 12,
         },
         padding: 12,
-        borderColor: 'rgba(0, 0, 0, 0.1)',
+        borderColor: "rgba(0, 0, 0, 0.1)",
         borderWidth: 1,
         callbacks: {
           label: (context) => {
@@ -113,65 +129,68 @@ const BarChart = ({ data, selectedLabels, setSelectedLabels, displayMode, onLabe
         },
         ticks: {
           font: {
-            size: 12
+            size: 12,
           },
-          color: '#64748b',
-          padding: 8
-        }
+          color: "#64748b",
+          padding: 8,
+        },
       },
       y: {
         beginAtZero: true,
         grid: {
-          color: 'rgba(0, 0, 0, 0.06)',
+          color: "rgba(0, 0, 0, 0.06)",
         },
         ticks: {
           font: {
-            size: 12
+            size: 12,
           },
-          color: '#64748b',
+          color: "#64748b",
           padding: 8,
-          stepSize: 1
-        }
+          stepSize: 1,
+        },
       },
     },
     datasets: {
       bar: {
         barPercentage: 0.8,
-        categoryPercentage: 0.9
-      }
+        categoryPercentage: 0.9,
+      },
     },
     onClick: (event, elements) => {
       if (elements.length > 0) {
         const clickedIndex = elements[0].index;
         const clickedLabel = Object.keys(distributionData)[clickedIndex];
-        
+
         // 선택된 라벨 토글
         const newSelectedLabels = selectedLabels.includes(clickedLabel)
-          ? selectedLabels.filter(label => label !== clickedLabel)
+          ? selectedLabels.filter((label) => label !== clickedLabel)
           : [...selectedLabels, clickedLabel];
-        
-        setSelectedLabels(newSelectedLabels);
-        
-        console.log(newSelectedLabels)
 
-        const selectedPoints = data.results?.filter(result => {
-          if (displayMode === 'prediction') {
-            return newSelectedLabels.includes(result.predictions?.prediction || '');
-          } else {
-            if (isObjectDetectionLabels(result.label)) {
-              return newSelectedLabels.includes(result.label.label);
+        setSelectedLabels(newSelectedLabels);
+
+        console.log(newSelectedLabels);
+
+        const selectedPoints =
+          data.results?.filter((result) => {
+            if (displayMode === "prediction") {
+              return newSelectedLabels.includes(
+                result.predictions?.prediction || ""
+              );
+            } else {
+              if (isObjectDetectionLabels(result.label)) {
+                return newSelectedLabels.includes(result.label.label);
+              }
+              return newSelectedLabels.includes(result.label || "");
             }
-            return newSelectedLabels.includes(result.label || '');
-          }
-        }) || [];
-        
+          }) || [];
+
         onLabelsSelect?.(selectedPoints);
       }
-    }
+    },
   };
 
   return (
-    <Card className="w-full h-full p-6 bg-white shadow-md">
+    <Card className="w-full h-full p-6 bg-white dark:bg-zinc-800 shadow-md">
       <div className="relative w-full h-full">
         <div className="absolute inset-0">
           <Bar data={chartData} options={options} />
