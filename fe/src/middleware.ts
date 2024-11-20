c5cd3411ab9b432be994a1f0f5de538552227d87
SnowyPacks <originpc@naver.com>
2024-11-20 13:12:47 +0900

    [#117] Fix: 인증 로직 변경

--- fe/src/middleware.ts
+++ fe/src/middleware.ts
@@ -1,30 +1,33 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-const publicRoutes = ["/login", "/signup", "/"];
+const publicRoutes = ['/login', '/signup', '/'];
 
 export async function middleware(request: NextRequest) {
-  const refreshTokenCookie = request.cookies.get("refreshToken");
+  const refreshTokenCookie = request.cookies.get('refreshToken');
   const path = request.nextUrl.pathname;
   const isPublicRoute = publicRoutes.includes(path);
   const res = NextResponse.next();
 
-  if (path === "/") {
+  if (path === '/') {
     return NextResponse.next();
   }
 
   if (refreshTokenCookie) {
+    // refresh token exists
     if (isPublicRoute) {
-      return NextResponse.redirect(new URL("/project", request.url));
+      // public route
+      return NextResponse.redirect(new URL('/project', request.url));
     } else return res;
   } else {
+    // refresh token does not exist
     if (isPublicRoute) {
       return res;
     } else {
-      return NextResponse.redirect(new URL("/login", request.url));
+      return NextResponse.redirect(new URL('/login', request.url));
     }
   }
 }
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
