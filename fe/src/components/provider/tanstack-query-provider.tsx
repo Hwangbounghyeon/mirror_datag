720e807286c50a7e63073204df2189b617f60411
SnowySparks <originpc@naver.com>
2024-11-20 14:42:21 +0900

    [#117] Fix: Tanstack Query provider변경

--- fe/src/components/provider/tanstack-query-provider.tsx
+++ fe/src/components/provider/tanstack-query-provider.tsx
@@ -1,14 +1,13 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 interface TanstackQueryProviderProps {
   children: React.ReactNode;
 }
 
-const queryClient = new QueryClient();
-
 const TanstackQueryProvider = ({ children }: TanstackQueryProviderProps) => {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
